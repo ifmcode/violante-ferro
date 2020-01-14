9285c6e5e6653ee4e3ce60ffbe3c451270a2e380
Isaac Fernandez <ifmcode@gmail.com>
2020-01-14 23:36:49 +0100

    terminada la maquetación de about, añadido el footer y empezado servicios

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,72 @@
-
+const $contactSection = $("#contact-section"),
+  $aboutSection = $("#about-section"),
+  $servicesSection = $("#services-section");
 
 $('document').ready(function () {
   bindEvents();
+  disableScroll();
+  positionateElements();
 });
 
 function bindEvents() {
-  $("#home-page .link-to-contact-page").click(homeToContactPageTransition);
-  $("#home-page .link-to-about-page").click(homeToAboutPageTransition);
-  $("#contact-page .link-to-home-page").click(contactToHomePageTransition);
-  $("#about-page .link-to-home-page").click(aboutToHomePageTransition);
+  $("#home-section .link-to-contact-section").click(homeToContactSectionTransition);
+  $("#home-section .link-to-about-section").click(homeToAboutSectionTransition);
+  $("#home-section .link-to-services-section").click(homeToServiceSectionTransition);
+  $("#contact-section .link-to-home-section").click(contactToHomeSectionTransition);
+  $("#about-section .link-to-home-section").click(aboutToHomeSectionTransition);
+  $("#services-section .link-to-home-section").click(servicesToHomeSectionTransition);
 }
 
-function homeToContactPageTransition() {
-  const contactPage = $("#contact-page");
+function positionateElements() {
+  const $email = $(".footer .email"),
+    $phoneNumber = $(".footer .phone-number");
+  $email.css("left",`-${$email.width()}px`);
+  $phoneNumber.css("right",`-${$phoneNumber.width()}px`);
+}
+
+function homeToContactSectionTransition() {
   const animation = new TimelineMax();
-  animation.to(contactPage, 0, {"top":"0", ease: Power2.easeInOut});
+  $contactSection.css("display","block");
+  animation.to($contactSection, 0, {"top":"0", ease: Power2.easeInOut});
 }
 
-function contactToHomePageTransition() {
-  const contactPage = $("#contact-page");
+function homeToAboutSectionTransition() {
   const animation = new TimelineMax();
-  animation.to(contactPage, 0, {"top":"100%", ease: Power2.easeInOut});
+  enableScroll();
+  animation.to($aboutSection, 0, {"left":"0", ease: Power2.easeInOut, onComplete: function(){
+    $("body").css("height", `${$aboutSection.height()}px`);
+	}});
 }
 
-function homeToAboutPageTransition() {
-  const contactPage = $("#about-page");
+function homeToServiceSectionTransition() {
   const animation = new TimelineMax();
-  animation.to(contactPage, 0, {"left":"0", ease: Power2.easeInOut});
+  enableScroll();
+  animation.to($servicesSection, 0, {"right":"0", ease: Power2.easeInOut});
 }
 
-function aboutToHomePageTransition() {
-  const contactPage = $("#about-page");
+function contactToHomeSectionTransition() {
   const animation = new TimelineMax();
-  animation.to(contactPage, 0, {"left":"-100%", ease: Power2.easeInOut});
+  animation.to($contactSection, 0, {"top":"100%", ease: Power2.easeInOut, onComplete: function(){
+    $contactSection.css("display","none");
+	}});
 }
 
+function aboutToHomeSectionTransition() {
+  const animation = new TimelineMax();
+  disableScroll()
+  animation.to($aboutSection, 0, {"left":"-100%", ease: Power2.easeInOut});
+}
+
+function servicesToHomeSectionTransition() {
+  const animation = new TimelineMax();
+  disableScroll()
+  animation.to($servicesSection, 0, {"right":"-100%", ease: Power2.easeInOut});
+}
+
+function enableScroll() {
+  $("body").css({"overflow-y": "auto", "height": "auto"});
+}
+
+function disableScroll() {
+  $("body").css({"overflow-y": "hidden", "height": "100vh"});
+}
