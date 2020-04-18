3f8dd3c1769b09111d988df898f7ec92576a0507
Isaac Fernandez <ifmcode@gmail.com>
2020-04-18 21:59:39 +0200

    añadido la notificación de privacidad y otros ajustes

--- scripts/main.js
+++ scripts/main.js
@@ -23,18 +23,34 @@ function bindEvents() {
   $('.link-to-services-section').click(goToMyIntentionSection);
   $('.home-logo-wrapper .logo').click(goToHomeSection);
   $('.home-logo-wrapper .logo-text').click(goToHomeSection);
-
-  $('#services-section #service-b .slider-arrow.left').click(moveToPreviousVideoSlide);
-  $('#services-section #service-b .slider-arrow.right').click(moveToNextVideoSlide);
+  $(".cookies-and-privacy-notification .close-button").click(hideCookieNotification);
 }
 
 function initializeVariables() {
   shownSection = homeSection;
   generalAnimation = new TimelineMax();
+  // acordarme de rellamar a la función cuando haga un resize de la pantalla
+  setHomeImageWidth();
+
 }
 
 function loadSplash() {
   splashAnimation1();
+}
+
+function setHomeImageWidth() {
+  const windowHeight = window.innerHeight,
+    windowWidth = window.innerWidth;
+  let imageHeight, imageWidth; 
+  if (windowWidth > windowHeight) {
+    imageWidth = `${windowWidth}px`;
+    imageHeight = 'auto';
+  } else {
+    imageWidth = 'auto';
+    imageHeight = `${windowHeight - 55}px`;
+  }
+  $("img.home-image").height(imageHeight);
+  $("img.home-image").width(imageWidth);
 }
 
 function resetGeneralAnimation() {
@@ -53,6 +69,7 @@ function removeSplash() {
   this.resetGeneralAnimation();
   generalAnimation.to($('.splash'), 1, {opacity: 0, ease: Power2.easeOut, onComplete: function() {
     $('.splash').css('display', 'none');
+    showCookieNotification();
   }});
 }
 
@@ -64,35 +81,40 @@ function disableScroll() {
   $('body').css({'overflow-y': 'hidden', 'height': '100vh'});
 }
 
+function showCookieNotification() {
+  $(".cookies-and-privacy-notification").removeClass("hide");
+}
+
+function hideCookieNotification() {
+  $(".cookies-and-privacy-notification").addClass("hide");
+}
+
 function goToHomeSection() {
   if (shownSection !== homeSection && !generalAnimation._active) {
-    updateActiveLink();
     navigate(homeSection);
   }
 }
 
 function goToContactSection(event) {
   if (shownSection !== contactSection && !generalAnimation._active) {
-    updateActiveLink(event);
-    navigate(contactSection);
+    navigate(contactSection, event);
   }
 }
 
 function goToAboutSection(event) {
   if (shownSection !== aboutSection && !generalAnimation._active) {
-    updateActiveLink(event);
-    navigate(aboutSection);
+    navigate(aboutSection, event);
   }
 }
 
 function goToMyIntentionSection(event) {
   if (shownSection !== myIntentionSection && !generalAnimation._active) {
-    updateActiveLink(event);
-    navigate(myIntentionSection);
+    navigate(myIntentionSection, event);
   }
 }
 
-function navigate(sectionToShow) {
+function navigate(sectionToShow, event) {
+  updateActiveLink(event);
   generalAnimation = new TimelineMax();
   sectionToShow.css('display','block');
   disableScroll();
@@ -102,7 +124,6 @@ function navigate(sectionToShow) {
     doSectionAnimation(sectionToShow);
   }
 
-  $("footer").toggleClass("grey-background", sectionToShow === contactSection);
   $("header").toggleClass("light-grey-background", sectionToShow === myIntentionSection);
 }
 
@@ -112,75 +133,29 @@ function updateActiveLink(event) {
 }
 
 function doSectionAnimation(sectionToShow) {
-  sectionToShow.css('z-index', '2');
+  sectionToShow.css({'z-index': '2', 'top':'100%'});
   shownSection.css('z-index', '1');
   generalAnimation.to(sectionToShow, 1, {'top':'0', ease: Power2.easeInOut}, 0)
   .to(shownSection, 1, {'top': '-50%', ease: Power2.easeInOut, onComplete: function () {
-    if (shownSection !== homeSection) {
-      shownSection.css({'display':'none', 'top':'100%'});
-    } else {
-      shownSection.css('display', 'none');
-    }
+    const cssRules = (shownSection !== homeSection)
+      ? {'display':'none', 'top':'100%'}
+      : {'display': 'none'};
+    shownSection.css(cssRules);
     shownSection = sectionToShow;
     enableScroll();
   }}, 0);
 }
 
 function doHomeAnimation(sectionToShow) {
+  const topOfScreen = $(window).scrollTop(),
+    bottomOfScreen = $(window).scrollTop() + window.innerHeight;
+
   sectionToShow.css('z-index', '1');
   shownSection.css('z-index', '2');
-  generalAnimation.to(sectionToShow, 1, {'top':'0', ease: Power2.easeInOut}, 0)
+  generalAnimation.to(sectionToShow, 1, {'top': `0`, ease: Power2.easeInOut}, 0)
   .to(shownSection, 1, {'top': '100%', ease: Power2.easeInOut, onComplete: function () {
     shownSection.css({'display':'none', 'top':'100%'});
     shownSection = sectionToShow;
     disableScroll();
   }}, 0);
-}
-
-function moveToPreviousVideoSlide() {
-  if (!generalAnimation._active) {
-    generalAnimation = new TimelineMax();
-    const slidesNumber = $('.video-slide').length - 1,
-      currentIndex = $('.video-slide.active').index(),
-      newIndex = ((currentIndex - 1) >= 0) ? (currentIndex - 1) : slidesNumber,
-      newVideo = $('.video-slide')[newIndex];
-      currentVideo = $('.video-slide')[currentIndex];
-      $currentSlide = $(currentVideo),
-      $newSlide = $(newVideo);
-  
-    $newSlide.css('left','-100%');
-    $newSlide.addClass('active');
-  
-    generalAnimation.to($currentSlide, 1, {'left':'100%', ease: Power2.easeInOut}, 0)
-      .to($newSlide, 1, {'left':'0', ease: Power2.easeInOut, onComplete: function() {
-        $currentSlide.removeClass('active');
-        $currentSlide.css('left','0');
-        currentVideo.pause();
-        currentVideo.currentTime = 0;
-      }}, 0);
-  }
-}
-
-function moveToNextVideoSlide() {
-  if (!generalAnimation._active) {
-    generalAnimation = new TimelineMax();
-    const slidesNumber = $('.video-slide').length - 1,
-      currentIndex = $('.video-slide.active').index(),
-      newIndex = (currentIndex + 1) <= slidesNumber ? (currentIndex + 1) : 0,
-      currentVideo = $('.video-slide')[currentIndex];
-      newVideo = $('.video-slide')[newIndex];
-      $currentSlide = $(currentVideo),
-      $newSlide = $(newVideo);
-  
-    $newSlide.css('left','100%');
-    $newSlide.addClass('active');
-
-    generalAnimation.to($currentSlide, 1, {'left':'-100%', ease: Power2.easeInOut}, 0)
-      .to($newSlide, 1, {'left':'0', ease: Power2.easeInOut, onComplete: function() {
-        $currentSlide.removeClass('active');
-        $currentSlide.css('left','0');
-        currentVideo.pause();
-        currentVideo.currentTime = 0;
-      }}, 0);
-  }
 }
